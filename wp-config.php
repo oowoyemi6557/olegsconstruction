14d859fdf23b403639d69320dfd8893ea496e63d
oowoyemi6557 <132838736+oowoyemi6557@users.noreply.github.com>
2023-11-05 13:51:14 -0500

    update wp-config

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'olegs_db' );
+define( 'DB_NAME', 'test_db' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
